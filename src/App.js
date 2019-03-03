fc8f6701c59a3bd65c1313accbe0a30c8fb545d7
Osbjorn Gulbranson <osbjorngulbranson@gmail.com>
2019-03-03 00:56:40 -0800

    commit 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello Doark! You are ready for heroku</div>;
+  return <div>ahhh</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
