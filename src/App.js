0e0e838a856da62cc21827e80a05776011c5e117
Osbjorn Gulbranson <osbjorngulbranson@gmail.com>
2019-03-03 00:37:07 -0800

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello React! You are ready for heroku</div>;
+  return <div>Hello Doark! You are ready for heroku</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
